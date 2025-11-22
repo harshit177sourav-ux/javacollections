6d1e5971097a2ed95c609541ce315c7caa6feb5d
harshit177sourav-ux <harhit177.sourav@gmail.com>
2025-11-22 22:13:45 +0530

    Testing Predicate with example 10

--- LambdaFunction.java
+++ LambdaFunction.java
@@ -3,5 +3,6 @@ import java.util.function.Predicate;
 public class LambdaFunction {
     public static void main(String[] args) {
         Predicate<Integer> predicate = x -> x % 2 ==0;
+        System.out.println(predicate.test(10));
     }
 }
