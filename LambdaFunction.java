4b998d005607fe041e503d354abc3c6ae2d9b1c6
Harshitsourav <harshit177.sourav@gmail.com>
2025-11-23 15:46:30 +0530

    udpated Supplier, Function and Consumer

--- LambdaFunction.java
+++ LambdaFunction.java
@@ -1,9 +1,21 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class LambdaFunction {
     public static void main(String[] args) {
         Predicate<Integer> predicate = x -> x % 2 ==0;
         System.out.println(predicate.test(10));
         System.out.println(predicate.test(5));
+
+        Supplier<Integer> supplier = () -> 20;
+        System.out.println(supplier.get());
+
+        Function<Integer, Integer> function = x -> x * 3;
+        System.out.println(function.apply(40));
+
+        Consumer<Integer> consumer = x -> System.out.println("Hi I'm Consumer and the value is: " + x);
+        consumer.accept(30);
     }
 }
